9835380317398333a467b5c1f0b9166644588633
takumakume <takuma.kume@gmail.com>
2021-05-14 00:11:56 +0900

    Brew formula update for kubectl-credentials-broker version 0.0.1

--- kubectl-credentials-broker.rb
+++ kubectl-credentials-broker.rb
@@ -10,22 +10,22 @@ class KubectlCredentialsBroker < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/takumakume/kubectl-credentials-broker/releases/download/0.0.1/kubectl-credentials-broker_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "f4598c3fdc5dfed2f2135e2b09d9a47103cd692277c0d08f901d083fdb7633a0"
+    sha256 "878e846fa827f796c396fd95fd87633182223e8b54ab7b8b35ae477b2126545c"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/takumakume/kubectl-credentials-broker/releases/download/0.0.1/kubectl-credentials-broker_0.0.1_Darwin_arm64.tar.gz"
-    sha256 "3072b85e5cdbdc1a2c0faf2fee13cb93615662f690f24b78e771ebd4b0e14932"
+    sha256 "132311c521b0e4719ca8f284856c65af68132785556aeb6f083d26c551155537"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/takumakume/kubectl-credentials-broker/releases/download/0.0.1/kubectl-credentials-broker_0.0.1_Linux_x86_64.tar.gz"
-    sha256 "ca8e66588c52f4a951ec0c798047e8a07c91b179b4084ed11349752c6446979c"
+    sha256 "5b778de1856afdee8037f443019ce4a965c3f7221bdba9f2b9f6bd4d853c3cc5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/takumakume/kubectl-credentials-broker/releases/download/0.0.1/kubectl-credentials-broker_0.0.1_Linux_arm64.tar.gz"
-    sha256 "94d4037e068e8184bea59a2f79e2dfd95fc2c7aaf1a4c211abd9988f968c5c78"
+    sha256 "c0ebf5314a896ecae018518c00c687cadb3f8215249f19a12e5add62a3e3fc04"
   end
 
   def install
-    bin.install "kubectl-credentials-broker"
+    bin.install "kubectl-credentials_broker"
   end
 end
